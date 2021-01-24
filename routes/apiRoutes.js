aefca9191bdc615bf6ed2687b9bf9a5c419350b5
Alfonso James <ajaeguy@gmail.com>
2021-01-23 20:28:40 -0500

    deploying

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { Router } = require("express");
 const Workout = require("../models/Workout");
 
 router.get("/workouts", (req, res) => {
--- server.js
+++ server.js
@@ -13,13 +13,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
-// app.use(require("./routes"));
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
